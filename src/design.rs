bb91122cc2bd95a32c51e3a46f407806c5801996
Albert Ford <albert@albertford.com>
2020-10-20 19:34:53 -0700

    Implement get_hand, get_table, is_game_over

--- src/design.rs
+++ src/design.rs
@@ -12,20 +12,3 @@ trait Game {
 
     fn get_table(&self) -> cards;
 }
-
-// A thought I had about Game::set_state:
-// The only time we will ever call game.set_state(None) is from inside
-// game.create_state(None).
-// So how about we move that code into create_state? (or into an init_state function)
-// Then we can get rid of the Option and match from set_state.
-// And I think we should avoid exposing an internal-only operation to a public API.
-
-// The game struct doesn't actually need to store the current player, right?
-// You talked about how it will always be Player::Me.
-// But I think we do need to store the order number of the first and last combination
-// played this trick. Storing the last order number is just convenient, but the first
-// one really matters because we need to distinguish between these two situations:
-//      Player A plays [5], Player B passes, Player A plays [6]
-//      Player A plays [5], Player B plays [6]
-// Actually, what do we do if it is the first turn of the game, or the opponent just passed?
-// How should we store that information?
--- src/game.rs
+++ src/game.rs
@@ -22,6 +22,11 @@ struct Game {
     /// The location of a card with id x is locations[x].
     locations: [Location; 42],
     current_player: Player,
+    /// Empty if game just started or the previous player just passed
+    last_combination: Vec<CardId>,
+    /// The order of the first combination played that has not yet been captured
+    /// so that we can efficiently search for the non-captured cards
+    current_start_order: usize,
 }
 
 enum CardValue {
@@ -34,6 +39,18 @@ enum CardValue {
     Wildcard {
         /// Wildcard rank is in the range 11..=13
         rank: usize,
+    }
+}
+
+impl CardValue {
+    fn point_value(&self) -> usize {
+        match self {
+            Self::Normal { rank, .. } => rank % 2,
+            Self::Wildcard { rank: 11 } => 2,
+            Self::Wildcard { rank: 12 } => 3,
+            Self::Wildcard { rank: 13 } => 5,
+            _ => panic!("Invalid wildcard rank"),
+        }
     }
 }
 
@@ -64,45 +81,126 @@ impl Game {
         let game = Game {
             locations: [Location::Haggis; 42],
             current_player: Player::Me,
+            last_combination: Vec::new(),
+            current_start_order: 0,
         };
-        game.set_state(qr_code);
+        match qr_code {
+            Some(data) => game.set_state(data),
+            None => game.init_state(),
+        };
         game
     }
 
-    fn set_state(&mut self, qr_code: Option<&[u8]>) {
-        match qr_code {
-            Some(_) => todo!(),
-            None => {
-                // Even though we only loop over the first 28 indices, we still
-                // need to shuffle all 36 normal cards so that the Haggis gets randomized.
-                let indices: Vec<_> = (0..36).collect();
-                indices.shuffle(&mut rand::thread_rng());
+    fn set_state(&mut self, qr_code: &[u8]) {
+        todo!();
+    }
 
-                for i in 0..14 {
-                    let rand_i = indices[i];
-                    self.locations[rand_i] = Location::Hand(Player::Me);
-                }
-                for i in 14..28 {
-                    let rand_i = indices[i];
-                    self.locations[rand_i] = Location::Hand(Player::Opponent);
-                }
+    fn init_state(&mut self) {
+        // Even though we only loop over the first 28 indices, we still
+        // need to shuffle all 36 normal cards so that the Haggis gets randomized.
+        let indices: Vec<_> = (0..36).collect();
+        indices.shuffle(&mut rand::thread_rng());
 
-                // Cleaner version of the for loops:
-                for &i in &indices[0..14] {
-                    self.locations[i] = Location::Hand(Player::Me);
-                }
-                for &i in &indices[14..28] {
-                    self.locations[i] = Location::Hand(Player::Opponent);
-                }
+        // Cleaner version of the for loops:
+        for &i in &indices[0..14] {
+            self.locations[i] = Location::Hand(Player::Me);
+        }
+        for &i in &indices[14..28] {
+            self.locations[i] = Location::Hand(Player::Opponent);
+        }
 
-                // Also I added the locations of the wild cards
-                for i in 36..39 {
-                    self.locations[i] = Location::Hand(Player::Me);
-                }
-                for i in 39..42 {
-                    self.locations[i] = Location::Hand(Player::Opponent);
+        for i in 36..39 {
+            self.locations[i] = Location::Hand(Player::Me);
+        }
+        for i in 39..42 {
+            self.locations[i] = Location::Hand(Player::Opponent);
+        }
+    }
+
+    fn get_hand(&self) -> Vec<CardId> {
+        let mut hand = Vec::new();
+        for (i, location) in self.locations.iter().enumerate() {
+            if let Location::Hand(Player::Me) = location {
+                hand.push(CardId(i));
+            }
+        }
+        hand
+    }
+
+    /// Return the non-captured cards of the table
+    fn get_table(&self) -> Vec<Vec<CardId>> {
+        if self.last_combination.is_empty() {
+            return Vec::new();
+        }
+
+        let current_end_location = self.locations[self.last_combination[0].0];
+        let current_end_order = match current_end_location {
+            Location::Table { order, captured_by: None } => order,
+            _ => panic!("Invalid self.last_combination"),
+        };
+
+        let mut combinations = vec![Vec::new(); 1 + current_end_order - self.current_start_order];
+
+        for (i, location) in self.locations.iter().enumerate() {
+            if let Location::Table { captured_by: None, order } = location {
+                combinations[order - self.current_start_order].push(CardId(i));
+            }
+        }
+        combinations
+    }
+
+    /// If game is over, return (my_score, opponent_score)
+    fn is_game_over(&self) -> Option<(usize, usize)> {
+        let mut my_card_count = 0;
+        let mut opponent_card_count = 0;
+
+        for location in self.locations.iter() {
+            match location {
+                Location::Hand(Player::Me) => my_card_count += 1,
+                Location::Hand(Player::Opponent) => opponent_card_count += 1,
+                _ => {}
+            };
+        }
+
+        if my_card_count == 0 || opponent_card_count == 0 {
+            let mut my_score = 0;
+            let mut opponent_score = 0;
+
+            let mut winner_of_hand_bonus = 0;
+
+            // The winner of the hand scores 5 points for each card in her opponent's hand.
+            // Remember, the wild cards count as part of the hand.
+            winner_of_hand_bonus += 5 * (my_card_count + opponent_card_count);
+
+            for (i, location) in self.locations.iter().enumerate() {
+                let card_id = CardId(i);
+                match location {
+                    // All point  cards (i.e., any 3, 5, 7, 9, J, Q, or K), captured during trick play,
+                    // are  scored by the capturing player.
+                    Location::Table { captured_by: Some(Player::Me), .. } => {
+                        my_score += card_id.to_value().point_value();
+                    }
+                    Location::Table { captured_by: Some(Player::Opponent), .. } => {
+                        opponent_score += card_id.to_value().point_value();
+                    }
+                    // Point cards left in the opponent's hand and any point cards found in the
+                    // Haggis are scored by the player who won the hand.
+                    Location::Hand(..) => {
+                        winner_of_hand_bonus += card_id.to_value().point_value();
+                    }
+                    _ => {}
                 }
             }
+
+            if my_card_count == 0 {
+                my_score += winner_of_hand_bonus;
+            } else {
+                opponent_score += winner_of_hand_bonus;
+            }
+
+            Some((my_score, opponent_score))
+        } else {
+            None
         }
     }
 }

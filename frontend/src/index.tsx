27a08c2b6cc7625d381654c6eea926c8456592e0
Albert Ford <albert@albertford.com>
2020-11-22 19:54:40 -0800

    Finish writing render() of react components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,31 @@ import * as ReactDOM from 'react-dom';
 
 type Stage = "before game" | "play" | "wait" | "game over";
 
-class App extends React.Component<{}, {}> {
+type AppState = {
+    stage: Stage,
+    my_score: number,
+    opponent_score: number,
+    qrUrl: string,
+};
+
+class App extends React.Component<{}, AppState> {
+    constructor() {
+        super({});
+        this.state = {
+            stage: "before game",
+            my_score: 0,
+            opponent_score: 0,
+            qrUrl: "",
+        };
+    }
+
     render() {
         return (
-            <>
-                <CardGrid stage="before game" />
-                <Sidebar />
-                <Scores />
-            </>
+            <div id="app">
+                <CardGrid stage={this.state.stage} />
+                <Sidebar stage={this.state.stage} qrUrl={this.state.qrUrl} my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
+                <Scores my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
+            </div>
         );
     }
 }
@@ -23,6 +40,7 @@ class CardGrid extends React.Component<CardGridProps> {
     render() {
         let rankLabels = [];
         let suitLabels = [];
+        let wildcardLabels = ['J', 'Q', 'K'];
         for (let rank = 2; rank <= 10; rank++) {
             rankLabels.push(<span>{rank}</span>);
         }
@@ -45,8 +63,8 @@ class CardGrid extends React.Component<CardGridProps> {
             <div id="ranks"><span></span>{rankLabels}</div>
             <div id="suits">{suitLabels}</div>
             <div id="normal_cards">{normalCards}</div>
-            <div id="my_wildcards">{myWilcards}</div>
-            <div id="opponent_wildcards">{opponentWildcards}</div>
+            <div id="my_wildcards">{myWilcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
+            <div id="opponent_wildcards">{opponentWildcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
         </div>;
     }
 }
@@ -71,39 +89,89 @@ class Card extends React.Component<CardProps> {
     }
 }
 
-class Sidebar extends React.Component {
+type SidebarProps = {
+    stage: Stage,
+    my_score: number,
+    opponent_score: number,
+    qrUrl: string,
+};
+
+class Sidebar extends React.Component<SidebarProps> {
     render() {
-        return (
-            <>
-                <QRDisplay />
-                <Button />
-                <QRReader />
-            </>
-        );
+        switch (this.props.stage) {
+            case "before game":
+                return <>
+                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                    <QRReader />
+                </>;
+            case "play":
+                return <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+            case "wait":
+                return <>
+                    <QRDisplay qrUrl={this.props.qrUrl} />
+                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                    <QRReader />
+                </>;
+            case "game over":
+                return <>
+                    <QRDisplay qrUrl={this.props.qrUrl} />
+                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                </>;
+        }
     }
 }
 
-class QRDisplay extends React.Component {
+type QRDisplayProps = {
+    qrUrl: string,
+};
+
+class QRDisplay extends React.Component<QRDisplayProps> {
     render() {
-        return <div></div>;
+        return <img id="qr_display" src={this.props.qrUrl} />;
     }
 }
 
-class Button extends React.Component {
+type ButtonProps = {
+    stage: Stage,
+    my_score: number,
+    opponent_score: number,
+};
+
+class Button extends React.Component<ButtonProps> {
     render() {
-        return <div></div>;
+        switch (this.props.stage) {
+            case 'before game':
+                return <div id="button">start</div>;
+            case 'play':
+                return <div id="button">play</div>;
+            case 'wait':
+                return <div id="button">wait</div>;
+            case 'game over':
+                if (this.props.my_score > this.props.opponent_score) {
+                    return <div id="button">you won</div>;
+                } else if (this.props.my_score < this.props.opponent_score) {
+                    return <div id="button">you lost</div>;
+                } else {
+                    return <div id="button">you tied</div>;
+                }
+        }
     }
 }
 
 class QRReader extends React.Component {
     render() {
-        return <div></div>;
+        return <div id="qr_reader"></div>;
     }
 }
 
-class Scores extends React.Component {
+type ScoresProps = {
+    my_score: number,
+    opponent_score: number,
+};
+
+class Scores extends React.Component<ScoresProps> {
     render() {
-        return <div></div>;
+        return <div id="scores">{this.props.my_score}pts / {this.props.opponent_score}pts</div>;
     }
 }
 

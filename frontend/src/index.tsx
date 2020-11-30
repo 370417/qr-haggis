2378851c9cabb594c085a6e6bbfa858955043865
Albert Ford <albert@albertford.com>
2020-11-29 21:55:54 -0800

    Fix passing rules and add interactivity to cards and sidebar button

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,13 +5,14 @@ import * as ReactDOM from 'react-dom';
 
 import('../dist/qr_haggis').then(module => {
 
-    console.log(module.Game.new());
-
     type AppState = {
         stage: GameStage,
-        my_score: number,
-        opponent_score: number,
+        myScore: number,
+        opponentScore: number,
         qrUrl: string,
+        game: Game,
+        canPlay: boolean,
+        selectedCards: Set<number>,
     };
 
     class App extends React.Component<{}, AppState> {
@@ -19,18 +20,57 @@ import('../dist/qr_haggis').then(module => {
             super({});
             this.state = {
                 stage: module.GameStage.BeforeGame,
-                my_score: 0,
-                opponent_score: 0,
+                myScore: 0,
+                opponentScore: 0,
                 qrUrl: "",
+                game: module.Game.new(),
+                canPlay: false,
+                selectedCards: new Set(),
             };
+            this.buttonHandler = this.buttonHandler.bind(this);
+            this.cardHandler = this.cardHandler.bind(this);
+        }
+
+        buttonHandler() {
+            switch (this.state.stage) {
+                case module.GameStage.BeforeGame:
+                    this.setState({
+                        stage: module.GameStage.Play,
+                    });
+                    break;
+                case module.GameStage.Play:
+                    if (this.state.canPlay) {
+                        this.state.game.play_cards(Uint32Array.from(this.state.selectedCards));
+                        this.setState({
+                            stage: this.state.game.game_stage(),
+                        });
+                    } else {
+                        alert("You did not select a valid card combination.");
+                    }
+                    break;
+            }
+        }
+
+        cardHandler(cardId: number) {
+            if (this.state.selectedCards.has(cardId)) {
+                this.state.selectedCards.delete(cardId);
+                this.setState({
+                    selectedCards: new Set(this.state.selectedCards),
+                });
+            } else {
+                this.state.selectedCards.add(cardId);
+                this.setState({
+                    selectedCards: new Set(this.state.selectedCards),
+                });
+            }
         }
 
         render() {
             return (
                 <div id="app">
-                    <CardGrid stage={this.state.stage} />
-                    <Sidebar stage={this.state.stage} qrUrl={this.state.qrUrl} my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
-                    <Scores my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
+                    <CardGrid stage={this.state.stage} selectedCards={this.state.selectedCards} cardHandler={this.cardHandler} game={this.state.game} />
+                    <Sidebar stage={this.state.stage} canPlay={this.state.canPlay} buttonHandler={this.buttonHandler} qrUrl={this.state.qrUrl} myScore={this.state.myScore} opponentScore={this.state.opponentScore} />
+                    <Scores myScore={this.state.myScore} opponentScore={this.state.opponentScore} />
                 </div>
             );
         }
@@ -38,6 +78,9 @@ import('../dist/qr_haggis').then(module => {
 
     type CardGridProps = {
         stage: GameStage,
+        game: Game,
+        selectedCards: Set<number>,
+        cardHandler: (cardId: number) => void,
     };
 
     class CardGrid extends React.Component<CardGridProps> {
@@ -55,13 +98,29 @@ import('../dist/qr_haggis').then(module => {
             let myWilcards = [];
             let opponentWildcards = [];
             for (let key = 0; key < 36; key++) {
-                normalCards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+                normalCards.push(<Card
+                    key={key}
+                    cardHandler={this.props.cardHandler}
+                    frontendState={this.props.game.card_frontend_state(key)}
+                    cardId={key}
+                    selected={this.props.selectedCards.has(key)}
+                    stage={this.props.stage} />);
             }
             for (let key = 36; key < 39; key++) {
-                myWilcards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+                myWilcards.push(<Card key={key}
+                    cardHandler={this.props.cardHandler}
+                    frontendState={this.props.game.card_frontend_state(key)}
+                    cardId={key}
+                    selected={this.props.selectedCards.has(key)}
+                    stage={this.props.stage} />);
             }
             for (let key = 39; key < 42; key++) {
-                opponentWildcards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+                opponentWildcards.push(<Card key={key}
+                    cardHandler={this.props.cardHandler}
+                    frontendState={this.props.game.card_frontend_state(key)}
+                    cardId={key}
+                    selected={this.props.selectedCards.has(key)}
+                    stage={this.props.stage} />);
             }
             return <div id="card_grid">
                 <div id="ranks"><span></span>{rankLabels}</div>
@@ -78,24 +137,35 @@ import('../dist/qr_haggis').then(module => {
         frontendState: CardFrontendState,
         selected: boolean,
         stage: GameStage,
+        cardHandler: (cardId: number) => void,
     };
 
     class Card extends React.Component<CardProps> {
         render() {
+            if (this.props.stage == module.GameStage.BeforeGame) {
+                return <div className="card">{this.props.frontendState}</div>;
+            }
             let className = "card";
             if (this.props.selected) {
                 className += " selected";
             }
             className += ` ${this.props.frontendState}`;
-            return <div className={className}>{this.props.cardId}</div>;
+
+            if (this.props.stage == module.GameStage.Play) {
+                return <div className={className} onClick={() => this.props.cardHandler(this.props.cardId)}>{this.props.frontendState}</div>;
+            }
+
+            return <div className={className}>{this.props.frontendState}</div>;
         }
     }
 
     type SidebarProps = {
         stage: GameStage,
-        my_score: number,
-        opponent_score: number,
+        myScore: number,
+        opponentScore: number,
         qrUrl: string,
+        canPlay: boolean,
+        buttonHandler: () => void,
     };
 
     class Sidebar extends React.Component<SidebarProps> {
@@ -103,21 +173,21 @@ import('../dist/qr_haggis').then(module => {
             switch (this.props.stage) {
                 case module.GameStage.BeforeGame:
                     return <>
-                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                        <Button stage={this.props.stage} canPlay={this.props.canPlay} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                         <QRReader />
                     </>;
                 case module.GameStage.Play:
-                    return <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                    return <Button stage={this.props.stage} canPlay={this.props.canPlay} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                 case module.GameStage.Wait:
                     return <>
                         <QRDisplay qrUrl={this.props.qrUrl} />
-                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                        <Button stage={this.props.stage} canPlay={this.props.canPlay} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                         <QRReader />
                     </>;
                 case module.GameStage.GameOver:
                     return <>
                         <QRDisplay qrUrl={this.props.qrUrl} />
-                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                        <Button stage={this.props.stage} canPlay={this.props.canPlay} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                     </>;
             }
         }
@@ -135,23 +205,28 @@ import('../dist/qr_haggis').then(module => {
 
     type ButtonProps = {
         stage: GameStage,
-        my_score: number,
-        opponent_score: number,
+        myScore: number,
+        opponentScore: number,
+        canPlay: boolean,
+        buttonHandler: () => void,
     };
 
     class Button extends React.Component<ButtonProps> {
         render() {
             switch (this.props.stage) {
                 case module.GameStage.BeforeGame:
-                    return <div id="button">start</div>;
+                    return <div id="button" className="enabled" onClick={this.props.buttonHandler}>start</div>;
                 case module.GameStage.Play:
+                    if (this.props.canPlay) {
+                        return <div id="button" className="enabled" onClick={this.props.buttonHandler}>play</div>;
+                    }
                     return <div id="button">play</div>;
                 case module.GameStage.Wait:
                     return <div id="button">wait</div>;
                 case module.GameStage.GameOver:
-                    if (this.props.my_score > this.props.opponent_score) {
+                    if (this.props.myScore > this.props.opponentScore) {
                         return <div id="button">you won</div>;
-                    } else if (this.props.my_score < this.props.opponent_score) {
+                    } else if (this.props.myScore < this.props.opponentScore) {
                         return <div id="button">you lost</div>;
                     } else {
                         return <div id="button">you tied</div>;
@@ -167,13 +242,13 @@ import('../dist/qr_haggis').then(module => {
     }
 
     type ScoresProps = {
-        my_score: number,
-        opponent_score: number,
+        myScore: number,
+        opponentScore: number,
     };
 
     class Scores extends React.Component<ScoresProps> {
         render() {
-            return <div id="scores">{this.props.my_score}pts / {this.props.opponent_score}pts</div>;
+            return <div id="scores">{this.props.myScore}pts / {this.props.opponentScore}pts</div>;
         }
     }
 

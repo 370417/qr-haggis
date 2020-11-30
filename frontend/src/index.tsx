506b0b05e47872639196d7b4e9ae68dc9063eb16
Albert Ford <albert@albertford.com>
2020-11-29 20:18:17 -0800

    Fix errors in importing types from backend to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,178 +1,182 @@
+import type { Game, GameStage, CardFrontendState } from '../dist/qr_haggis';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { CardFrontendState, GameStage, Game } from '../dist/qr_haggis';
 
-type AppState = {
-    stage: GameStage,
-    my_score: number,
-    opponent_score: number,
-    qrUrl: string,
-};
+import('../dist/qr_haggis').then(module => {
 
-class App extends React.Component<{}, AppState> {
-    constructor() {
-        super({});
-        this.state = {
-            stage: GameStage.BeforeGame,
-            my_score: 0,
-            opponent_score: 0,
-            qrUrl: "",
-        };
-    }
+    console.log(module.Game.new());
 
-    render() {
-        return (
-            <div id="app">
-                <CardGrid stage={this.state.stage} />
-                <Sidebar stage={this.state.stage} qrUrl={this.state.qrUrl} my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
-                <Scores my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
-            </div>
-        );
-    }
-}
+    type AppState = {
+        stage: GameStage,
+        my_score: number,
+        opponent_score: number,
+        qrUrl: string,
+    };
 
-type CardGridProps = {
-    stage: GameStage,
-};
-
-class CardGrid extends React.Component<CardGridProps> {
-    render() {
-        let rankLabels = [];
-        let suitLabels = [];
-        let wildcardLabels = ['J', 'Q', 'K'];
-        for (let rank = 2; rank <= 10; rank++) {
-            rankLabels.push(<span>{rank}</span>);
+    class App extends React.Component<{}, AppState> {
+        constructor() {
+            super({});
+            this.state = {
+                stage: module.GameStage.BeforeGame,
+                my_score: 0,
+                opponent_score: 0,
+                qrUrl: "",
+            };
         }
-        for (let suit = 0; suit < 4; suit++) {
-            suitLabels.push(<span>{suit}</span>);
-        }
-        let normalCards = [];
-        let myWilcards = [];
-        let opponentWildcards = [];
-        for (let key = 0; key < 36; key++) {
-            normalCards.push(<Card key={key} frontendState={CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
-        }
-        for (let key = 36; key < 39; key++) {
-            myWilcards.push(<Card key={key} frontendState={CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
-        }
-        for (let key = 39; key < 42; key++) {
-            opponentWildcards.push(<Card key={key} frontendState={CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
-        }
-        return <div id="card_grid">
-            <div id="ranks"><span></span>{rankLabels}</div>
-            <div id="suits">{suitLabels}</div>
-            <div id="normal_cards">{normalCards}</div>
-            <div id="my_wildcards">{myWilcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
-            <div id="opponent_wildcards">{opponentWildcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
-        </div>;
-    }
-}
 
-type CardProps = {
-    cardId: number,
-    frontendState: CardFrontendState,
-    selected: boolean,
-    stage: GameStage,
-};
-
-class Card extends React.Component<CardProps> {
-    render() {
-        let className = "card";
-        if (this.props.selected) {
-            className += " selected";
-        }
-        className += ` ${this.props.frontendState}`;
-        return <div className={className}>{this.props.cardId}</div>;
-    }
-}
-
-type SidebarProps = {
-    stage: GameStage,
-    my_score: number,
-    opponent_score: number,
-    qrUrl: string,
-};
-
-class Sidebar extends React.Component<SidebarProps> {
-    render() {
-        switch (this.props.stage) {
-            case GameStage.BeforeGame:
-                return <>
-                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
-                    <QRReader />
-                </>;
-            case GameStage.Play:
-                return <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
-            case GameStage.Wait:
-                return <>
-                    <QRDisplay qrUrl={this.props.qrUrl} />
-                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
-                    <QRReader />
-                </>;
-            case GameStage.GameOver:
-                return <>
-                    <QRDisplay qrUrl={this.props.qrUrl} />
-                    <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
-                </>;
+        render() {
+            return (
+                <div id="app">
+                    <CardGrid stage={this.state.stage} />
+                    <Sidebar stage={this.state.stage} qrUrl={this.state.qrUrl} my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
+                    <Scores my_score={this.state.my_score} opponent_score={this.state.opponent_score} />
+                </div>
+            );
         }
     }
-}
 
-type QRDisplayProps = {
-    qrUrl: string,
-};
+    type CardGridProps = {
+        stage: GameStage,
+    };
 
-class QRDisplay extends React.Component<QRDisplayProps> {
-    render() {
-        return <img id="qr_display" src={this.props.qrUrl} />;
-    }
-}
-
-type ButtonProps = {
-    stage: GameStage,
-    my_score: number,
-    opponent_score: number,
-};
-
-class Button extends React.Component<ButtonProps> {
-    render() {
-        switch (this.props.stage) {
-            case GameStage.BeforeGame:
-                return <div id="button">start</div>;
-            case GameStage.Play:
-                return <div id="button">play</div>;
-            case GameStage.Wait:
-                return <div id="button">wait</div>;
-            case GameStage.GameOver:
-                if (this.props.my_score > this.props.opponent_score) {
-                    return <div id="button">you won</div>;
-                } else if (this.props.my_score < this.props.opponent_score) {
-                    return <div id="button">you lost</div>;
-                } else {
-                    return <div id="button">you tied</div>;
-                }
+    class CardGrid extends React.Component<CardGridProps> {
+        render() {
+            let rankLabels = [];
+            let suitLabels = [];
+            let wildcardLabels = ['J', 'Q', 'K'];
+            for (let rank = 2; rank <= 10; rank++) {
+                rankLabels.push(<span>{rank}</span>);
+            }
+            for (let suit = 0; suit < 4; suit++) {
+                suitLabels.push(<span>{suit}</span>);
+            }
+            let normalCards = [];
+            let myWilcards = [];
+            let opponentWildcards = [];
+            for (let key = 0; key < 36; key++) {
+                normalCards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+            }
+            for (let key = 36; key < 39; key++) {
+                myWilcards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+            }
+            for (let key = 39; key < 42; key++) {
+                opponentWildcards.push(<Card key={key} frontendState={module.CardFrontendState.Unknown} cardId={key} selected={false} stage={this.props.stage} />);
+            }
+            return <div id="card_grid">
+                <div id="ranks"><span></span>{rankLabels}</div>
+                <div id="suits">{suitLabels}</div>
+                <div id="normal_cards">{normalCards}</div>
+                <div id="my_wildcards">{myWilcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
+                <div id="opponent_wildcards">{opponentWildcards}{wildcardLabels.map(label => <span>{label}</span>)}</div>
+            </div>;
         }
     }
-}
 
-class QRReader extends React.Component {
-    render() {
-        return <div id="qr_reader"></div>;
+    type CardProps = {
+        cardId: number,
+        frontendState: CardFrontendState,
+        selected: boolean,
+        stage: GameStage,
+    };
+
+    class Card extends React.Component<CardProps> {
+        render() {
+            let className = "card";
+            if (this.props.selected) {
+                className += " selected";
+            }
+            className += ` ${this.props.frontendState}`;
+            return <div className={className}>{this.props.cardId}</div>;
+        }
     }
-}
 
-type ScoresProps = {
-    my_score: number,
-    opponent_score: number,
-};
+    type SidebarProps = {
+        stage: GameStage,
+        my_score: number,
+        opponent_score: number,
+        qrUrl: string,
+    };
 
-class Scores extends React.Component<ScoresProps> {
-    render() {
-        return <div id="scores">{this.props.my_score}pts / {this.props.opponent_score}pts</div>;
+    class Sidebar extends React.Component<SidebarProps> {
+        render() {
+            switch (this.props.stage) {
+                case module.GameStage.BeforeGame:
+                    return <>
+                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                        <QRReader />
+                    </>;
+                case module.GameStage.Play:
+                    return <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                case module.GameStage.Wait:
+                    return <>
+                        <QRDisplay qrUrl={this.props.qrUrl} />
+                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                        <QRReader />
+                    </>;
+                case module.GameStage.GameOver:
+                    return <>
+                        <QRDisplay qrUrl={this.props.qrUrl} />
+                        <Button stage={this.props.stage} my_score={this.props.my_score} opponent_score={this.props.opponent_score} />
+                    </>;
+            }
+        }
     }
-}
 
-let game = Game.new();
-console.log(game);
+    type QRDisplayProps = {
+        qrUrl: string,
+    };
 
-ReactDOM.render(<App />, document.body);
+    class QRDisplay extends React.Component<QRDisplayProps> {
+        render() {
+            return <img id="qr_display" src={this.props.qrUrl} />;
+        }
+    }
+
+    type ButtonProps = {
+        stage: GameStage,
+        my_score: number,
+        opponent_score: number,
+    };
+
+    class Button extends React.Component<ButtonProps> {
+        render() {
+            switch (this.props.stage) {
+                case module.GameStage.BeforeGame:
+                    return <div id="button">start</div>;
+                case module.GameStage.Play:
+                    return <div id="button">play</div>;
+                case module.GameStage.Wait:
+                    return <div id="button">wait</div>;
+                case module.GameStage.GameOver:
+                    if (this.props.my_score > this.props.opponent_score) {
+                        return <div id="button">you won</div>;
+                    } else if (this.props.my_score < this.props.opponent_score) {
+                        return <div id="button">you lost</div>;
+                    } else {
+                        return <div id="button">you tied</div>;
+                    }
+            }
+        }
+    }
+
+    class QRReader extends React.Component {
+        render() {
+            return <div id="qr_reader"></div>;
+        }
+    }
+
+    type ScoresProps = {
+        my_score: number,
+        opponent_score: number,
+    };
+
+    class Scores extends React.Component<ScoresProps> {
+        render() {
+            return <div id="scores">{this.props.my_score}pts / {this.props.opponent_score}pts</div>;
+        }
+    }
+
+    ReactDOM.render(<App />, document.body);
+});
+
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
 
 module.exports = {
     entry: './src/index.tsx',
@@ -16,13 +18,22 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: 'static', to: path.join(__dirname, 'dist') },
+                // { from: 'wasm', to: path.join(__dirname, 'dist') },
             ],
         }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, "../backend"),
+            outDir: "../frontend/dist",
+            outName: "qr_haggis",
+            withTypescript: true,
+        }),
     ],
+    devtool: "source-map",
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9000
+        port: 9000,
+        // writeToDisk: true,
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],

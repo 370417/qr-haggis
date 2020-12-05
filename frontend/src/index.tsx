3e8220d797f141dc8119e2e2e056947aeafc3dd4
Albert Ford <albert@albertford.com>
2020-12-05 00:47:47 -0800

    Use custom drop target implementation instead of react-dropzone

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,7 +14,7 @@ import('../dist/qr_haggis').then(module => {
 
     type AppState = {
         stage: GameStage,
-        qrPixels: Uint8ClampedArray,
+        qrObjectUrl: string | undefined,
         myScore: number,
         opponentScore: number,
 
@@ -27,7 +27,7 @@ import('../dist/qr_haggis').then(module => {
             super({});
             this.state = {
                 stage: module.GameStage.BeforeGame,
-                qrPixels: Uint8ClampedArray.from([]),
+                qrObjectUrl: undefined,
                 myScore: 0,
                 opponentScore: 0,
                 isSelectionValid: false,
@@ -49,19 +49,40 @@ import('../dist/qr_haggis').then(module => {
                     if (this.state.isSelectionValid) {
                         game.play_cards(Uint32Array.from(this.state.selectedCards));
                         let scores = game.calculate_score();
-                        let qrPixels = game.to_qr_code(QR_WIDTH, QR_HEIGHT);
+
                         this.setState({
                             stage: game.game_stage(),
                             selectedCards: new Set(),
                             myScore: scores[0],
                             opponentScore: scores[1],
-                            qrPixels,
+                            qrObjectUrl: undefined,
                         });
+
+                        this.renderQRCode();
                     } else {
                         alert("You did not select a valid card combination.");
                     }
                     break;
             }
+        }
+
+        renderQRCode() {
+            let qrPixels = game.to_qr_code(QR_WIDTH, QR_HEIGHT);
+
+            let canvas = document.createElement('canvas');
+            let size = Math.sqrt(qrPixels.length / 4);
+            canvas.width = size;
+            canvas.height = size;
+
+            let ctx = canvas.getContext('2d');
+            let imageData = new ImageData(qrPixels, canvas.width, canvas.height);
+            ctx?.putImageData(imageData, 0, 0);
+
+            canvas.toBlob(blob => {
+                this.setState({
+                    qrObjectUrl: URL.createObjectURL(blob),
+                });
+            });
         }
 
         cardHandler(cardId: number) {
@@ -93,10 +114,22 @@ import('../dist/qr_haggis').then(module => {
         render() {
             const scores = game.calculate_score();
             return (
-                <div id="app">
-                    <CardGrid stage={this.state.stage} selectedCards={this.state.selectedCards} cardHandler={this.cardHandler} game={game} />
-                    <Sidebar stage={this.state.stage} isSelectionValid={this.state.isSelectionValid} readQRHandler={this.readQRHandler} buttonHandler={this.buttonHandler} qrPixels={this.state.qrPixels} myScore={this.state.myScore} opponentScore={this.state.opponentScore} />
-                    <Scores myScore={scores[0]} opponentScore={scores[1]} />
+                <div id="app" className={game.am_player_1() ? "player1" : "player2"}>
+                    <CardGrid
+                        stage={this.state.stage}
+                        selectedCards={this.state.selectedCards}
+                        cardHandler={this.cardHandler}
+                        game={game} />
+                    <Scores
+                        myScore={scores[0]}
+                        opponentScore={scores[1]} />
+                    <Sidebar stage={this.state.stage}
+                        isSelectionValid={this.state.isSelectionValid}
+                        readQRHandler={this.readQRHandler}
+                        buttonHandler={this.buttonHandler}
+                        qrObjectUrl={this.state.qrObjectUrl}
+                        myScore={this.state.myScore}
+                        opponentScore={this.state.opponentScore} />
                 </div>
             );
         }
@@ -169,19 +202,20 @@ import('../dist/qr_haggis').then(module => {
     class Card extends React.Component<CardProps> {
         render() {
             if (this.props.stage == module.GameStage.BeforeGame) {
-                return <div className="card"></div>;
+                return <div className="card state0"></div>;
             }
+
             let className = "card";
             if (this.props.selected) {
                 className += " selected";
             }
-            className += ` ${this.props.frontendState}`;
+            className += ` state${this.props.frontendState}`;
 
             if (this.props.stage == module.GameStage.Play && this.props.frontendState == module.CardFrontendState.InMyHand) {
-                return <div className={className} onClick={() => this.props.cardHandler(this.props.cardId)}>{this.props.frontendState}</div>;
+                return <div className={className} onClick={() => this.props.cardHandler(this.props.cardId)}></div>;
             }
 
-            return <div className={className}>{this.props.frontendState}</div>;
+            return <div className={className}></div>;
         }
     }
 
@@ -189,7 +223,7 @@ import('../dist/qr_haggis').then(module => {
         stage: GameStage,
         myScore: number,
         opponentScore: number,
-        qrPixels: Uint8ClampedArray,
+        qrObjectUrl: string | undefined,
         isSelectionValid: boolean,
         buttonHandler: () => void,
         readQRHandler: (imageData: ArrayBuffer) => void,
@@ -201,19 +235,19 @@ import('../dist/qr_haggis').then(module => {
                 case module.GameStage.BeforeGame:
                     return <>
                         <Button stage={this.props.stage} isSelectionValid={this.props.isSelectionValid} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
-                        <QRReader readQRHandler={this.props.readQRHandler} />
+                        <QRReader2 readQRHandler={this.props.readQRHandler} />
                     </>;
                 case module.GameStage.Play:
                     return <Button stage={this.props.stage} isSelectionValid={this.props.isSelectionValid} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                 case module.GameStage.Wait:
                     return <>
-                        <QRDisplay qrPixels={this.props.qrPixels} />
+                        {this.props.qrObjectUrl !== undefined ? <QRDisplay qrObjectUrl={this.props.qrObjectUrl} /> : ""}
                         <Button stage={this.props.stage} isSelectionValid={this.props.isSelectionValid} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
-                        <QRReader readQRHandler={this.props.readQRHandler} />
+                        <QRReader2 readQRHandler={this.props.readQRHandler} />
                     </>;
                 case module.GameStage.GameOver:
                     return <>
-                        <QRDisplay qrPixels={this.props.qrPixels} />
+                        {this.props.qrObjectUrl !== undefined ? <QRDisplay qrObjectUrl={this.props.qrObjectUrl} /> : ""}
                         <Button stage={this.props.stage} isSelectionValid={this.props.isSelectionValid} buttonHandler={this.props.buttonHandler} myScore={this.props.myScore} opponentScore={this.props.opponentScore} />
                     </>;
             }
@@ -221,23 +255,12 @@ import('../dist/qr_haggis').then(module => {
     }
 
     type QRDisplayProps = {
-        qrPixels: Uint8ClampedArray,
+        qrObjectUrl: string,
     };
 
     class QRDisplay extends React.Component<QRDisplayProps> {
         render() {
-            let canvas = document.createElement('canvas');
-            let clampedArray = this.props.qrPixels;
-            let size = Math.sqrt(clampedArray.length / 4);
-            canvas.width = size;
-            canvas.height = size;
-            let ctx = canvas.getContext('2d');
-            if (ctx) {
-                console.log(clampedArray, canvas);
-                let imageData = new ImageData(clampedArray, canvas.width, canvas.height);
-                ctx.putImageData(imageData, 0, 0);
-            }
-            return <img id="qr_display" src={canvas.toDataURL()} />;
+            return <img id="qr_display" src={this.props.qrObjectUrl} />;
         }
     }
 
@@ -299,7 +322,7 @@ import('../dist/qr_haggis').then(module => {
         render() {
             return <Dropzone onDrop={this.onDrop}>
                 {({ getRootProps, getInputProps }) => (
-                    <div id="qr_reader" {...getRootProps()}>
+                    <div id="qr_reader2" {...getRootProps()}>
                         <input {...getInputProps()} />
                         <p>Drag 'n' drop some files here, or click to select files</p>
                     </div>
@@ -316,6 +339,46 @@ import('../dist/qr_haggis').then(module => {
     class Scores extends React.Component<ScoresProps> {
         render() {
             return <div id="scores">{this.props.myScore}pts / {this.props.opponentScore}pts</div>;
+        }
+    }
+
+    class QRReader2 extends React.Component<QRReaderProps> {
+        dragOver(event: React.DragEvent) {
+            if (event.dataTransfer.types.includes("text/uri-list")) {
+                event.dataTransfer.dropEffect = "copy";
+                event.preventDefault();
+            }
+        }
+
+        drop(event: React.DragEvent) {
+            event.preventDefault();
+
+            if (event.dataTransfer.types.includes("text/uri-list")) {
+                const uri = event.dataTransfer.getData("text/uri-list");
+
+                const img = new Image();
+                img.crossOrigin = "Anonymous";
+                img.onload = () => {
+                    const canvas = document.createElement("canvas");
+                    canvas.width = img.naturalWidth;
+                    canvas.height = img.naturalHeight;
+                    canvas.getContext("2d")?.drawImage(img, 0, 0);
+
+                    canvas.toBlob(blob => {
+                        blob?.arrayBuffer().then(arrayBuffer => {
+                            this.props.readQRHandler(arrayBuffer);
+                        });
+                    });
+                };
+                img.src = uri;
+            }
+        }
+
+        render() {
+            return <div id="qr_reader"
+                onDragOver={this.dragOver}
+                onDragEnter={this.dragOver}
+                onDrop={this.drop.bind(this)}></div>;
         }
     }
 

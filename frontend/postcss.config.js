e9ef92f93cdf03d80d8c546cb2bb1eee7e23f0f1
Albert Ford <albert@albertford.com>
2020-12-15 04:20:46 -0800

    Minify css more (remove variables at build time)

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     plugins: [
+        require('postcss-custom-properties')({
+            preserve: false,
+        }),
+        require('postcss-calc'),
         require('cssnano')({
             preset: 'advanced',
         }),
